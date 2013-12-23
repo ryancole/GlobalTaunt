1fd8a36a893b70a87ee8e9dac38b34650371711f
Ryan Cole <ryan@rycole.com>
2013-12-22 22:29:44 -0600

    flesh out Game data more

--- GlobalTaunt.Model/Models/Game.cs
+++ GlobalTaunt.Model/Models/Game.cs
@@ -8,8 +8,6 @@ namespace GlobalTaunt.Model
 
         public int Level { get; set; }
 
-        public int MapId { get; set; }
-
         public int Spell1 { get; set; }
 
         public int Spell2 { get; set; }
@@ -30,6 +28,56 @@ namespace GlobalTaunt.Model
 
         public string GameType { get; set; }
 
+        public GameMap MapId { get; set; }
+
+        public List<GamePlayer> FellowPlayers { get; set; }
+
+        public List<GameRawStats> Statistics { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return string.Format("Game {0}, on {1}", GameId, MapId);
+        }
+
+        #endregion
+    }
+
+    public class GamePlayer
+    {
+        #region Properties
+
+        public int TeamId { get; set; }
+
+        public int ChampionId { get; set; }
+
+        public long SummonerId { get; set; }
+
+        #endregion
+    }
+
+    public class GameRawStats
+    {
+        #region Properties
+
+        public int Id { get; set; }
+
+        public int Value { get; set; }
+
+        public string Name { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return string.Format("{0} => {1}", Name, Value);
+        }
+
         #endregion
     }
 
@@ -42,5 +90,16 @@ namespace GlobalTaunt.Model
         public List<Game> Games { get; set; }
 
         #endregion
+    }
+
+    public enum GameMap
+    {
+        SummonersRiftSummer = 1,
+        SummonersRiftAutumn = 2,
+        ProvingGrounds = 3,
+        TwistedTreelineOriginal = 4,
+        CrystalScar = 8,
+        TwistedTreeline = 10,
+        HowlingAbyss = 12
     }
 }
